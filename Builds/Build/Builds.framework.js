6fd6a7eafd079eedf92f854a50d847329c9b4fb2
huxianyin <huxy214@gmail.com>
2022-11-11 18:43:57 +0900

    fix bugs

--- Builds/Build/Builds.framework.js
+++ Builds/Build/Builds.framework.js
@@ -1293,7 +1293,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2460816;
+STATICTOP = STATIC_BASE + 2460752;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3367,7 +3367,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2460816;
+var STATIC_BUMP = 2460752;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
